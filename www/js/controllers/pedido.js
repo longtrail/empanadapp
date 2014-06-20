b6a4ded34a342946aec2ccfa30a8fba1f884d0e5
Alan Szpigiel <alanszp@gmail.com>
2014-06-20 18:06:43 -0300

    Cambia la app de estado, ahora apasa a la de guido cuando haces click en la card

--- www/js/controllers/pedido.js
+++ www/js/controllers/pedido.js
@@ -1,18 +1,19 @@
 var app = angular.module('empanadapp');
-app.controller('PedidoCtrl', ["$scope", "PersonasService", "PopUpBuilder", function($scope, Personas, PopUpBuilder) {
+app.controller('PedidoCtrl', ["$scope", "PersonasService", "PopUpBuilder", "$state", function($scope, Personas, PopUpBuilder, $state) {
 	$scope.personas = Personas.getPersonas();
 
 	$scope.agregar = Personas.add;
 
+	$scope.empaname = function(id){
+		$state.go('tab.pedido-persona', {personaId: id})
+	};
+
 	PopUpBuilder.submit = function(data) {
     return $scope.agregar(data); 
   };
-
   PopUpBuilder.placeholder = "Escribi el nombre de la persona";
-
   PopUpBuilder.title = "Agregar persona";
 
-	$scope.showPopup = PopUpBuilder.build // Returns the function
-
+	$scope.showPopup = PopUpBuilder.build // Returns the function	
 }]);
 
