1464b8ba0757337f0e5b13a75d44b1aa63a7922e
Guido Kuznicki <guido.kuznicki@graion.com>
2014-07-06 17:30:33 -0300

    termine pedidoPersona. Falta un cacho de diseño

--- www/js/controllers/pedidoPersona.js
+++ www/js/controllers/pedidoPersona.js
@@ -1,6 +1,6 @@
 var app = angular.module('empanadapp');
 app.controller('PedidoPersonaCtrl',  ["$scope", "PersonasService", "EmpanadasService", "PopUpBuilder", "$state", function($scope, pServ, eServ, popUp, $stateParam) {
-	
+
 	//harcodeo ID y agrego persona. desp sacar
 	var id = 1;
 	personaNueva = pServ.add('hitler');
@@ -8,7 +8,20 @@ app.controller('PedidoPersonaCtrl',  ["$scope", "PersonasService", "EmpanadasSer
 	//Recibo persona de la vista de personas
 	//var id = $stateParam(personaId);
 
+	/*Para popUp*/
+	popUp.submit = function(data) {
+   	 return eServ.add(data); 
+  	};
+  	popUp.placeholder = "Escribi el nombre del gusto";
+ 	popUp.title = "Agregar Gusto";
+	$scope.showPopup = popUp.build;
+	/**/
+
+	/*Datos a mostrar desde un principio*/
 	$scope.persona = pServ.getPersona(id);
 	$scope.empanadas = eServ.getEmpanadas();
 	$scope.cantEmpanadas = $scope.persona.totalEmpanadas();
+
+	/*Agregar/Sacar empanadas*/
+	
 }]);
